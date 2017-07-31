aa4111a13b86b38c8386bc788c2817935c42f6e4
glennfield20 <ladesmaglenngarfield@gmail.com>
2017-07-31 18:41:04 +0800

    Render main react component

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import path from 'path'
+import webpack from 'webpack'
+import webpackMiddleware from 'webpack-dev-middleware'
+import webpackConfig from '../webpack.config.dev.js'
 
 // initialize express
 let app = express()
 
+// use webpack
+app.use(webpackMiddleware(webpack(webpackConfig)))
 // handle all routes
 
 app.get('/*', (req, res) => {
