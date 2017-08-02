b075403ca72f4b8a13e6a9b902daee490283fdd2
glennfield20 <ladesmaglenngarfield@gmail.com>
2017-08-02 22:24:36 +0800

    React router and basic navigation between pages

--- client/index.js
+++ client/index.js
@@ -1,5 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Route, BrowserRouter, Switch } from 'react-router-dom'
+import Greetings from './components/Greetings'
+import SignupPage from './components/signup/SignupPage'
 import App from './components/App'
 
-render(<App />, document.getElementById('app'))
+render(
+	<BrowserRouter>
+		<App>
+			<Switch>
+				<Route exact path="/" component={Greetings} />
+				<Route path="/signup" component={SignupPage} />
+			</Switch>	
+		</App>
+	</BrowserRouter>
+	,
+	document.getElementById('app')
+)
