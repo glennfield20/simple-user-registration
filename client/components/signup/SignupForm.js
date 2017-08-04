2bc102d570fdd30d5140d6ba4b6351c17f064a30
glennfield20 <ladesmaglenngarfield@gmail.com>
2017-08-04 10:57:16 +0800

    Adds server side validations.

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -3,6 +3,7 @@ import timezones from '../../data/timezones'
 import map from 'lodash/map'
 import axios from 'axios'
 import PropTypes from 'prop-types'
+import classnames from 'classnames'
 
 class SignupForm extends React.Component {
 	constructor(props) {
@@ -12,7 +13,9 @@ class SignupForm extends React.Component {
 			email: '',
 			password: '',
 			passwordConfirmation: '',
-			timezone: ''
+			timezone: '',
+			errors: {},
+			isLoading: false
 		}
 
 		this.onChange = this.onChange.bind(this)
@@ -25,10 +28,17 @@ class SignupForm extends React.Component {
 
 	onSubmit(e) {
 		e.preventDefault()
+		this.setState({ errors: {}, isLoading: true })
 		this.props.userSignupRequest(this.state)
+			.then(() => {})
+			.catch((error) => {
+				this.setState({ errors: error.response.data, isLoading: false })
+			})
 	}
 
 	render () {
+		const { errors } = this.state
+
 		const options = map(timezones, (val, key) => 
 			<option key={val} value={val}>{key}</option>
 		)
@@ -37,7 +47,7 @@ class SignupForm extends React.Component {
 			<form onSubmit={this.onSubmit}>
 				<h1>Join our community!</h1>
 
-				<div className="form-group">
+				<div className={ classnames("form-group", { 'has-error': errors.username }) }>
 					<label className="control-label">Username</label>
 					<input
 						value={this.state.username}
@@ -46,9 +56,10 @@ class SignupForm extends React.Component {
 						name="username"
 						className="form-control"
 					/>
+					{ errors.username && <span className='help-block'>{errors.username}</span> }
 				</div>
 
-				<div className="form-group">
+				<div className={ classnames("form-group", { 'has-error': errors.email }) }>
 					<label className="control-label">Email</label>
 					<input
 						value={this.state.email}
@@ -57,9 +68,10 @@ class SignupForm extends React.Component {
 						name="email"
 						className="form-control"
 					/>
+					{ errors.email && <span className='help-block'>{errors.email}</span> }
 				</div>
 
-				<div className="form-group">
+				<div className={ classnames("form-group", { 'has-error': errors.password }) }>
 					<label className="control-label">Password</label>
 					<input
 						value={this.state.password}
@@ -68,9 +80,10 @@ class SignupForm extends React.Component {
 						name="password"
 						className="form-control"
 					/>
+					{ errors.password && <span className='help-block'>{errors.password}</span> }
 				</div>
 
-				<div className="form-group">
+				<div className={ classnames("form-group", { 'has-error': errors.passwordConfirmation }) }>
 					<label className="control-label">Password Confirmation</label>
 					<input
 						value={this.state.passwordConfirmation}
@@ -79,9 +92,10 @@ class SignupForm extends React.Component {
 						name="passwordConfirmation"
 						className="form-control"
 					/>
+					{ errors.passwordConfirmation && <span className='help-block'>{errors.passwordConfirmation}</span> }
 				</div>
 
-				<div className="form-group">
+				<div className={ classnames("form-group", { 'has-error': errors.timezone }) }>
 					<label className="control-label">Timezone</label>
 
 					<select
@@ -93,10 +107,11 @@ class SignupForm extends React.Component {
 						<option value="">Choose Your Timezone</option>
 						{options}
 					</select>
+					{ errors.timezone && <span className='help-block'>{ errors.timezone }</span> }
 				</div>
 
 				<div className="form-group">
-					<button className="btn btn-primary btn-lg">
+					<button disabled={this.state.isLoading} className="btn btn-primary btn-lg">
 						Sign up
 					</button>
 				</div>
--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 import path from 'path'
 import webpack from 'webpack'
 import webpackMiddleware from 'webpack-dev-middleware'
 import webpackConfig from '../webpack.config.dev.js'
 import webpackHotMiddleware from 'webpack-hot-middleware'
+import users from './routes/users'
 
 // initialize express
 let app = express()
+
+app.use(bodyParser.json())
+
+// Routes 
+app.use('/api/users', users)
 
 // use webpack
 const compiler = webpack(webpackConfig)
